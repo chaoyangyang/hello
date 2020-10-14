5a272000a5b8c8e6f5c581ee4be752533096e872
cyy <1102941383@qq.ccom>
2020-10-14 09:15:43 +0800

    修改文件

--- src/hello.java
+++ src/hello.java
@@ -10,17 +10,7 @@ public class hello {
         System.out.println("ccccccccccc");
         System.out.println("abcd");
         System.out.println("aaaaaaaa");
-        system.out.println("你好！")
-        System.out.println("解决冲突！");
-        System.out.println("先commit后pull");
-        System.out.println("小测试。。。");
-        System.out.println("1111111111111111111111111");
 
-
-        System.out.println("nihao");
-        System.out.println("1111111111111111111111111");
-        System.out.println(22222222222);
-        System.out.println("nihao");
-        System.out.println(66666);
+        System.out.println("哈哈哈哈哈哈哈哈哈");
     }
 }
